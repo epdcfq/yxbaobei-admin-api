092b08a5ddf0f63ba18996afd38ceb5cd4588a00
fuqiang <340093856@qq.com>
2019-12-31 16:44:58 +0800

    修改域名首页返回值

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '';
 });
 // Route::any('/user/login', function(Request $request) {
 // 	exit(json_encode(['code'=>20000, 'data'=>'admin_token_web']));
