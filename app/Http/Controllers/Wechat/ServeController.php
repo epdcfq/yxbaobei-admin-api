3b3635b97610cc027bee16901ceb30a828125eb0
fuqiang <340093856@qq.com>
2020-01-14 08:45:52 +0800

    消息测试

--- app/Http/Controllers/Wechat/ServeController.php
+++ app/Http/Controllers/Wechat/ServeController.php
@@ -26,13 +26,14 @@ class ServeController extends Controller
      */
     public function serve(Request $request)
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        Log::info('wechat/server request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
         // $app->server->push(function($message){
         //     return "欢迎关注 overtrue！";
         // });
         $app->server->push(function ($message) {
+            Log::info('$app->server->push', $message);
             $this->wxmessage->newMessage($message);
             switch ($message['MsgType']) {
                 case 'event':
